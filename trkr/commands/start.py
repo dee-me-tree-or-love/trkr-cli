7da437e5097a24761706c40975f83dccee1e8035
Dmitry <dmitry-orlov@hotmail.com>
2017-03-04 11:45:40 +0100

    tasks starting

--- trkr/commands/start.py
+++ trkr/commands/start.py
@@ -3,24 +3,46 @@
 from .base import Base
 from .locatedb import checkdbloc
 
-from clint.textui import puts, colored
+from clint.textui import puts, colored, indent
 import sqlite3
 from json import dumps
 
-
+from trkr.globals import StoredDBProc, getdbloc
 
 class Start(Base):
 
-    # def closeold(self):
-    #      dbcon = sqlite3.connect()
-    #
-    # def starttask(self,tn):
-    #     print()
+    dbprocedures = StoredDBProc()
+
+    # a generalized wrapper for the command execution
+    def executedbcommand(self,text):
+        dbcon = sqlite3.connect(getdbloc())
+        cur = dbcon.cursor()
+        cur.execute(text)
+        dbcon.commit()
+        dbcon.close()
+
+
+    def closeold(self):
+        self.executedbcommand(self.dbprocedures.closeold)
+
+    def starttask(self,tn):
+        self.executedbcommand(self.dbprocedures.startnewtask(tn))
+
 
     def run(self):
-        # if not checkdbloc():
-        #     puts(colored.red('The database does not exist, run initdb to make sure'))
-        # else:
-        #     print('You have started the new task: ',self.options['<task-name>'])
+        if not checkdbloc():
+            #verify that the db exists
+            puts(colored.red('The database does not exist, run initdb to make sure'))
+        else:
+            # do starting of the new task here
 
-        print('You supplied the following options:', dumps(self.options, indent=2, sort_keys=True))
+            # close the old unfinished tasks first:
+            try:
+                self.closeold()
+                taskname = self.options['<task-name>']
+                self.starttask(taskname)
+                print('You have started the new task: ',taskname)
+            except Exception as e:
+                print("db connection error:")
+                print(e)
+        # print('You supplied the following options:', dumps(self.options, indent=2, sort_keys=True))
--- trkr/globals/dbprocedures.py
+++ trkr/globals/dbprocedures.py
@@ -1,13 +1,15 @@
+"""The class to store the database procedures"""
 
+# the class contains either string attributes or the methods that return strings
 class StoredDBProc:
 
-    updtaskstocurtime = 'UPDATE tasks'\
-                        'SET time_finished = CURRENT_TIME'\
-                        'WHERE time_finished IS NULL;'
+    closeold = 'UPDATE tasks '\
+                'SET time_finished = CURRENT_TIME ' \
+                'WHERE time_finished IS NULL;'
 
     def startnewtask(self,tn):
-        return 'INSERT INTO tasks'\
-               '(title,time_started)' \
-               'VALUES ('+tn+',CURRENT_TIME);'
+        return 'INSERT INTO tasks '\
+               '(title,time_started) ' \
+               'VALUES ("'+tn+'",CURRENT_TIME);'
 
 
